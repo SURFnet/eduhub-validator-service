68bf8427d305e226f93c1f9cdfe1059234e667f2
Joost Diepenmaat <joost@zeekat.nl>
2025-01-22 10:32:46 +0100

    Add some basic logging

--- src/nl/surf/eduhub/validator/service/api.clj
+++ src/nl/surf/eduhub/validator/service/api.clj
@@ -17,9 +17,10 @@
 ;; <https://www.gnu.org/licenses/>.
 
 (ns nl.surf.eduhub.validator.service.api
-  (:require [clojure.string :as str]
+  (:require [clojure.string :as string]
             [compojure.core :refer [GET POST]]
             [compojure.route :as route]
+            [clojure.tools.logging :as log]
             [nl.jomco.http-status-codes :as http-status]
             [nl.surf.eduhub.validator.service.authentication :as auth]
             [nl.surf.eduhub.validator.service.checker :as checker]
@@ -87,7 +88,7 @@
 
 (defn private-routes [{:keys [introspection-endpoint-url introspection-basic-auth
                               allowed-client-ids] :as config} auth-disabled]
-  (let [allowed-client-id-set (set (str/split allowed-client-ids #","))
+  (let [allowed-client-id-set (set (string/split allowed-client-ids #","))
         auth-opts             {:auth-disabled (boolean auth-disabled)}]
     (-> (compojure.core/routes
           (POST "/endpoints/:endpoint-id/config" [endpoint-id]
@@ -98,9 +99,34 @@
         (wrap-json-response)
         (wrap-defaults api-defaults))))
 
+(defn error-message-from-response
+  [{:keys [status body]}]
+  (if (<= 400 status)
+    (or (:message body)
+        (let [msg (string/replace (str body) #"\s+" " ")]
+          (if (<= 300 (count msg))
+            (subs msg 0 300)
+            msg)))))
+
+(defn wrap-log
+  [handler]
+  (fn [request]
+    (try (let [{:keys [status body] :as response} (handler request)]
+           (if (<= 500 status)
+             (log/error (str (:status response) " " (:request-method request) " " (:uri request)))
+             (log/info (str (:status response) " " (:request-method request) " " (:uri request))))
+           (when-let [msg (error-message-from-response response)]
+             (log/error msg))
+           response)
+         (catch Exception e
+           (log/error e
+                      (str "Exception handling "  (:request-method request) " " (:uri request) ": " (ex-message e)))
+           (throw e)))))
+
 ;; Compose the app from the routes and the wrappers. Authentication can be disabled for testing purposes.
 (defn compose-app [config auth-disabled]
-  (compojure.core/routes
-    (public-routes config)
-    (private-routes config auth-disabled)
-    (route/not-found "Not Found")))
+  (-> (compojure.core/routes
+       (public-routes config)
+       (private-routes config auth-disabled)
+       (route/not-found "Not Found"))
+      (wrap-log)))
